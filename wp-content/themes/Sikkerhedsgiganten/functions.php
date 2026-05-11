7f8f1be7a85adc29df350fafb6e3873eb460b297
AndersJ2812 <andjoe02@easv365.dk>
2026-05-11 10:52:12 +0200

    Gutenberg slet

--- wp-content/themes/Sikkerhedsgiganten/functions.php
+++ wp-content/themes/Sikkerhedsgiganten/functions.php
@@ -10,4 +10,13 @@ function mytheme_enqueue_styles() {
     );
 }
 add_action('wp_enqueue_scripts', 'mytheme_enqueue_styles');
+function bst_load_resources() {
+    wp_enqueue_style("main-css", get_template_directory_uri() . "/style.css");
+}
+add_action("wp_enqueue_scripts", "bst_load_resources");
+function disable_gutenberg() {
+    remove_post_type_support('page', 'editor');
+
+}
+add_action('init', 'disable_gutenberg');
 ?>
