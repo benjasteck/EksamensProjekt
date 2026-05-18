650f0774eb659a41480f53525784d20beb8ab212
AndersJ2812 <andjoe02@easv365.dk>
2026-05-18 09:27:34 +0200

    Header update

--- wp-content/themes/Sikkerhedsgiganten/header.php
+++ wp-content/themes/Sikkerhedsgiganten/header.php
@@ -77,6 +77,14 @@ $profiltitel = get_field("profil_titel");
 $profilundersidetitel = get_field("profil_underside_titel");
 $favorittertitel = get_field("favoritter_titel");
 $kurvtitel = get_field("kurv_titel");
+$navforside= get_field("nav-forside");
+$navomos= get_field("nav-om-os");
+$navkontakt= get_field("nav-kontakt");
+$navkundeservice= get_field("nav-kundeservice");
+$navshop= get_field("nav-shop");
+$naverhvervskunde= get_field("nav-erhvervskunde");
+
+
 
 
 
@@ -88,12 +96,12 @@ $kurvtitel = get_field("kurv_titel");
 <body <?php body_class() ?>>
     <nav id="nav">
         <div class="navTop">
-            <div class="navLeftSide">
+            <div class="navLeftSide"> 
                 <ul>
-                    <li><a href="#forside">Forside</a></li>
-                    <li><a href="#omos">Om os</a></li>
-                    <li><a href="#kontakt">Kontakt</a></li>
-                    <li><a href="#kundeservice">Kundeservice</a></li>
+                    <li><a href="#forside"><?php echo($navforside); ?></a></li>  
+                    <li><a href="#omos"><?php echo($navomos); ?></a></li>
+                    <li><a href="#kontakt"><?php echo($navkontakt); ?></a></a></li>
+                    <li><a href="#kundeservice"><?php echo($navkundeservice); ?></a></li>    
                 </ul>
             </div>
             <div class="navMiddle">
@@ -105,8 +113,8 @@ $kurvtitel = get_field("kurv_titel");
             </div>
             <div class="navRightSide">
                 <ul>
-                    <li><a href="#shop">Shop</a></li>
-                    <li><a href="#erhvervskunde">Erhvervskunde</a></li>
+                    <li><a href="#shop"><?php echo($navshop); ?></a></li>
+                    <li><a href="#erhvervskunde"><?php echo($naverhvervskunde); ?></a></li>
                     <li><a href="#favoritter"><img src="<?php echo esc_url($Navhjerteikon["url"]); ?>"
                                 alt="<?php echo $Navhjerteikon ["alt"]?>"></a></li>
                     <li><a href="#kurv"><img src="<?php echo esc_url($Navkurvikon["url"]); ?>"
