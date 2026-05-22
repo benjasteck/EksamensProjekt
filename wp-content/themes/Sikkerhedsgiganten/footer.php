fbccd545506223813d46e5db57a38556aaa1f3c2
AndersJ2812 <andjoe02@easv365.dk>
2026-05-22 11:18:49 +0200

    Footer tekster opdateret

--- wp-content/themes/Sikkerhedsgiganten/footer.php
+++ wp-content/themes/Sikkerhedsgiganten/footer.php
@@ -9,6 +9,21 @@ $footerinstagramikon = get_field("footer_instagram_ikon", $footer_post_id);
 $footerfacebooknikon = get_field("footer_facebook_ikon", $footer_post_id);
 $footerlinkedinikon  = get_field("footer_linkedin_ikon", $footer_post_id);
 $footerlogo          = get_field("footer_logo", $footer_post_id);
+$blivendelafnyhedsbrevettitel          = get_field("bliv_en_del_af_nyhedsbrevet_titel", $footer_post_id);
+$kontakttitel          = get_field("kontakt_titel", $footer_post_id);
+$tlfnr          = get_field("tlf_nr", $footer_post_id);
+$adresselokation          = get_field("adresse_lokation", $footer_post_id);
+$abningstidertitel          = get_field("abningstider_titel", $footer_post_id);
+$mantiltorsabningstider          = get_field("man_til_tors_abningstider", $footer_post_id);
+$fredagabningstider          = get_field("fredag_abningstider", $footer_post_id);
+$navigeringtitel          = get_field("navigering_titel", $footer_post_id);
+$forsidefooterlink          = get_field("forside_footer_link", $footer_post_id);
+$shopfooterlink          = get_field("shop_footer_link", $footer_post_id);
+$erhvervfooterlink          = get_field("erhverv_footer_link", $footer_post_id);
+$blogsfooterlink          = get_field("blogs_footer_link", $footer_post_id);
+$omosfooterlink          = get_field("om_os_footer_link", $footer_post_id);
+$kontaktfooterlink          = get_field("kontakt_footer_link", $footer_post_id);
+$socialt_titel          = get_field("socialt_titel", $footer_post_id);
 ?>
 
 
@@ -18,7 +33,7 @@ $footerlogo          = get_field("footer_logo", $footer_post_id);
     <div class="footerContent">
         <div class="footerContentLeft">
             <div class="footerContentTop">
-                <h2 class="nyhedsbrevTitelFooter">Bliv en del af nyhedbrevet</h2>
+                <h2 class="nyhedsbrevTitelFooter"><?php echo($blivendelafnyhedsbrevettitel); ?></h2>  
                 <form method="post">
                     <input type="email" name="Email" placeholder="Din Email">
                     <button type="submit" value=" ">
@@ -29,53 +44,54 @@ $footerlogo          = get_field("footer_logo", $footer_post_id);
             </div>
             <div class="footerContentBottom">
                 <div class="footerContentBottomContainer">
-                    <h2 class="titelFooter">Kontakt</h2>
+                    <h2 class="titelFooter"><?php echo($kontakttitel); ?></h2>  
                     <div class="footerText">
                         <img src="<?php echo esc_url($footermailikon["url"]); ?>"
                             alt="<?php echo $footermailikon ["alt"]?>">
-                        <p>info@sikkerhedsgiganten.dk</p>
+                        <p><?php echo($emailadressetitel); ?></p>
+
                     </div>
                     <div class="footerText">
                         <img src="<?php echo esc_url($footertlfikon["url"]); ?>"
                             alt="<?php echo $footertlfikon ["alt"]?>">
-                        <p>+45 71 99 32 60</p>
+                        <p><?php echo($tlfnr); ?></p>    
                     </div>
                     <div class="footerText">
                         <img src="<?php echo esc_url($footeradresseikon["url"]); ?>"
                             alt="<?php echo $footeradresseikon ["alt"]?>">
-                        <p>Lillebæltsvej 6 · A/B 6715 Esbjerg N</p>
+                        <p><?php echo($adresselokation); ?></p>   
                     </div>
                 </div>
                 <div class="footerContentBottomContainer">
-                    <h2 class="titelFooter">Åbningstider</h2>
+                    <h2 class="titelFooter"><?php echo($abningstidertitel); ?></h2>
                     <div class="footerText">
-                        <p>· Mandag til torsdag: 8:00 - 16:00</p>
+                        <p>· <?php echo($mantiltorsabningstider); ?></p>  
                     </div>
                     <div class="footerText">
-                        <p>· Fredag: 8:00 - 15.30</p>
+                        <p>· <?php echo($fredagabningstider); ?></p>  
                     </div>
                 </div>
                 <div class="footerContentBottomContainer">
-                    <h2 class="titelFooter">Navigering</h2>
+                    <h2 class="titelFooter"><?php echo($navigeringtitel); ?></h2>  
                     <div class="grid">
-                        <a href="http://sikkerhedsgiganten.local/">Forside</a>
-                        <a href="http://sikkerhedsgiganten.local/shop/">Shop</a>
-                        <a href="http://sikkerhedsgiganten.local/index.php/erhverv/">Erhverv</a>
-                        <a href="http://sikkerhedsgiganten.local/index.php/blogs/">Blogs</a>
-                        <a href="http://sikkerhedsgiganten.local/index.php/om-os/">Om Os</a>
-                        <a href="http://sikkerhedsgiganten.local/index.php/kontakt/">Kontakt</a>
+                        <a href="http://sikkerhedsgiganten.local/" aria-label="en knap ind til forsiden"><?php echo($forsidefooterlink); ?></a>  
+                        <a href="http://sikkerhedsgiganten.local/shop/" aria-label="en knap ind til shop siden"><?php echo($shopfooterlink); ?></a>  
+                        <a href="http://sikkerhedsgiganten.local/index.php/erhverv/" aria-label="en knap ind til erhvervs siden"><?php echo($erhvervfooterlink); ?></a>  
+                        <a href="http://sikkerhedsgiganten.local/index.php/blogs/" aria-label="en knap ind til blogs siden"><?php echo($blogsfooterlink); ?></a>  
+                        <a href="http://sikkerhedsgiganten.local/index.php/om-os/" aria-label="en knap ind til om os siden"><?php echo($omosfooterlink); ?></a>  
+                        <a href="http://sikkerhedsgiganten.local/index.php/kontakt/" aria-label="en knap ind til kontakt os siden"><?php echo($kontaktfooterlink); ?></a>  
                     </div>
                 </div>
                 <div class="footerContentBottomContainer">
-                    <h2 class="titelFooter">Socialt</h2>
+                    <h2 class="titelFooter"><?php echo($socialt_titel); ?></h2>  
                     <div class="footerText">
-                        <a href="https://www.instagram.com/sikkerhedsgigantendk/"><img
+                        <a href="https://www.instagram.com/sikkerhedsgigantendk/" aria-label="en knap ind til Sikkerhedsgiganten Instagram"><img
                                 src="<?php echo esc_url($footerinstagramikon["url"]); ?>"
                                 alt="<?php echo $footerinstagramikon ["alt"]?>" class="footerSocial"></a>
-                        <a href="https://www.facebook.com/sikkerhedsgiganten.dk"><img
+                        <a href="https://www.facebook.com/sikkerhedsgiganten.dk" aria-label="en knap ind til Sikkerhedsgiganten Facebook"><img
                                 src="<?php echo esc_url($footerfacebooknikon["url"]); ?>"
                                 alt="<?php echo $footerfacebooknikon ["alt"]?>" class="footerSocial"></a>
-                        <a href="https://www.linkedin.com/company/sikkerhedsgiganten/"><img
+                        <a href="https://www.linkedin.com/company/sikkerhedsgiganten/" aria-label="en knap ind til Sikkerhedsgiganten LinkedIn"><img
                                 src="<?php echo esc_url($footerlinkedinikon["url"]); ?>"
                                 alt="<?php echo $footerlinkedinikon ["alt"]?>" class="footerSocial"></a>
                     </div>
